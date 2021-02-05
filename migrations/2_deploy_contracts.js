d32ff0702be0334359ca8083fb4ce39772991c1f
Michael <stall84@gmail.com>
2021-02-05 17:12:39 -0500

    Initial Commit. First Round of Migrations to mock block-chain pending

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,19 @@
+const DappToken = artifacts.require('DappToken')
+const DaiToken = artifacts.require('DaiToken')
+const TokenFarm = artifacts.require('TokenFarm');
+
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock Dai Token Contract and then read its value
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+  // Deploy Dapp Token Contract and then read its value
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+  // Deploy all 3 contracts to the (mock) block-chain
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+  // Transfer all tokens to the TokenFarm (1 million)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+  // Transfer 100 Mock DAI tokens to investor
+  await daiToken.transfer(accounts[1], '100000000000000000000')
+};
